cf4a9a3720ddb4e78628e11075be75552019fb56
qiuyuanjun <18707097515@163.com>
2023-07-07 23:48:18 +0800

    完善词法解析器的nextToken方法代码

--- streamexpr-api/src/main/java/com/qiuyj/streamexpr/api/Lexer.java
+++ streamexpr-api/src/main/java/com/qiuyj/streamexpr/api/Lexer.java
@@ -7,5 +7,9 @@ package com.qiuyj.streamexpr.api;
  */
 public interface Lexer {
 
+    /**
+     * 得到下一个词法单元，即{@link Token}对象
+     * @return {@link Token}对象
+     */
     Token nextToken();
 }
--- streamexpr-core/src/main/scala/com/qiuyj/streamexpr/parser/StreamExpressionScanner.scala
+++ streamexpr-core/src/main/scala/com/qiuyj/streamexpr/parser/StreamExpressionScanner.scala
@@ -2,6 +2,8 @@ package com.qiuyj.streamexpr.parser
 
 import com.qiuyj.streamexpr.api.{Lexer, Token}
 
+import java.util.Objects
+
 /**
  * @author qiuyj
  * @since 2023-06-29
@@ -10,7 +12,32 @@ class StreamExpressionScanner(private[this] val sourceString: String) extends Le
 
   private[this] val tokenizer = new StreamExpressionTokenizer(sourceString)
 
+  /**
+   * 用于存储上一个token
+   */
+  private[this] var prevToken: Token = _
+
+  /**
+   * 用于存储预读的下一个token
+   */
+  private[this] var lookaheadToken: Token = _
+
+  /**
+   * 用于存储当前的token
+   */
+  private[this] var currentToken: Token = _
+
   override def nextToken: Token = {
-    tokenizer.readToken
+    if (Objects.nonNull(currentToken)) {
+      prevToken = currentToken
+    }
+    if (Objects.nonNull(lookaheadToken)) {
+      currentToken = lookaheadToken
+      lookaheadToken = null
+    }
+    else {
+      currentToken = tokenizer.readToken
+    }
+    currentToken
   }
 }
