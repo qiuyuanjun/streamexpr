5c96af55741c16ca4882f91f7ae8845d755e25ae
qiuyuanjun <18707097515@163.com>
2023-08-24 00:13:24 +0800

    优化Stream接口

--- streamexpr-core/src/main/scala/com/qiuyj/streamexpr/stream/AbstractStream.scala
+++ streamexpr-core/src/main/scala/com/qiuyj/streamexpr/stream/AbstractStream.scala
@@ -2,18 +2,16 @@ package com.qiuyj.streamexpr.stream
 import com.qiuyj.streamexpr.StreamExpression
 import com.qiuyj.streamexpr.StreamExpression.StreamOp
 
-import scala.collection.mutable
-
 /**
  * @author qiuyj
  * @since 2023-08-23
  */
-class AbstractStream(private[this] val prev: Stream) extends Stream {
+class AbstractStream(private[this] val head: Stream, private[this] val prev: Stream) extends Stream {
 
-  override def addIntermediateOps(intermediateOps: mutable.Seq[StreamOp]): Stream = {
+  override def addIntermediateOps(intermediateOps: collection.Seq[StreamOp]): Stream = {
     var stream: Stream = this
-    for (streamOp <- intermediateOps) {
-      stream = new AbstractStream(this)
+    for (intermediateOp <- intermediateOps) {
+      stream = new AbstractStream(head,this)
     }
     stream
   }
--- streamexpr-core/src/main/scala/com/qiuyj/streamexpr/stream/Stream.scala
+++ streamexpr-core/src/main/scala/com/qiuyj/streamexpr/stream/Stream.scala
@@ -2,8 +2,6 @@ package com.qiuyj.streamexpr.stream
 
 import com.qiuyj.streamexpr.StreamExpression.StreamOp
 
-import scala.collection.mutable
-
 /**
  * @author qiuyj
  * @since 2023-06-29
@@ -16,9 +14,9 @@ trait Stream {
    * @return 当前stream流对象
    */
   def addIntermediateOp(intermediateOp: StreamOp): Stream =
-    addIntermediateOps(mutable.Seq.apply(intermediateOp))
+    addIntermediateOps(Seq.apply(intermediateOp))
 
-  def addIntermediateOps(intermediateOps: mutable.Seq[StreamOp]): Stream
+  def addIntermediateOps(intermediateOps: collection.Seq[StreamOp]): Stream
 
   /**
    * 根据传入的终止操作计算当前流式操作的结果
